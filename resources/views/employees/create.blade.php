f36fe81fe0d01201bf2450dede163d9fb6f643c6
TYMtwilight <ore01100001binary@gmail.com>
2023-03-05 05:29:43 +0900

    ✅ Laravelで基本的なWebアプリを開発してみよう 第5章:データベース接続 の講義に使用したコードです。

---
+++ resources/views/employees/create.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Create | Employees</title> 
+  </head>
+  <body>
+    <h1>Create</h1>
+    <form action="/databaseapp/public/employees/store" method="post">
+      @csrf
+      <table>
+        <tr>
+          <th>氏名</th>
+          <td><input type="text" name="employee_name"></td>
+        </tr>
+        <tr>
+          <th>部署</th>
+          <td><input type="text" name="section"></td>
+        </tr>
+        <tr>
+          <th>基本給</th>
+          <td>
+            <input type="number" name="salary">
+          </td>
+        </tr>
+        <tr>
+          <th></th>
+          <td><input type="submit" value="送信"></td>
+        </tr>
+      </table>
+    </form>
+  </body>
+</html>
---
+++ resources/views/employees/store.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYP html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>Store</h1>
+    <p>登録が完了しました。</p>
+    <p><a href="/databaseapp/public/employees">
+    一覧へ
+    </a></p>
+  </body>
+</html>
